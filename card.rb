8274c5ffb5d0df0085a1aad119626415f0f7b0d9
Tobi1246 <92755680+Tobi1246@users.noreply.github.com>
2022-10-29 21:28:29 +0300

    Update card.rb

--- card.rb
+++ card.rb
@@ -1,12 +1,12 @@
 class Card
   attr_accessor :rank, :suit, :value
-     
+  # класс карт , сдесь создаём наминал, масти и значения
   def initialize(rank, suit, value)
     @rank = rank
     @suit = suit
     @value = value
   end
-  
+  # соёздаём сами карты и подсчитываем сразу их значения с условием на туза
   def generate_card(player)
     new_card = Card.new rank, suit, value
     player.card_in_hand << new_card
