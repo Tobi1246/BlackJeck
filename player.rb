caf7769314005cd00154324520a4d0ee5d01a65e
Tobi1246 <92755680+Tobi1246@users.noreply.github.com>
2022-10-29 21:31:49 +0300

    Update player.rb

--- player.rb
+++ player.rb
@@ -1,6 +1,6 @@
 class Player
   attr_accessor :bank, :card_in_hand, :name, :total
-
+# класс игрока в который мы помещаем наши карты , а так же баланс и имя игрока
   def initialize(name)
     @name = name
     @bank = 100
