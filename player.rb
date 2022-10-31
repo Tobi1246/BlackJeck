81301d4aa7ef784d05948a5a9c6d2eae4af335e2
Tobi1246 <92755680+Tobi1246@users.noreply.github.com>
2022-10-31 11:29:58 +0300

    Update player.rb

--- player.rb
+++ player.rb
@@ -1,6 +1,8 @@
 class Player
   attr_accessor :bank, :card_in_hand, :name, :total
 # класс игрока в который мы помещаем наши карты , а так же баланс и имя игрока
+  STEP_RATE = 10
+  MAX_POINTS = 21
   def initialize(name)
     @name = name
     @bank = 100
